ba1d21e7c1c11deee58a1573af69c0a76383dde6
swatisriva1 <40495467+swatisriva1@users.noreply.github.com>
2020-03-28 20:21:14 -0400

    Added pic field to form for updating profile info

--- tutor/forms.py
+++ tutor/forms.py
@@ -6,7 +6,7 @@ from django.contrib.auth.forms import UserChangeForm
 class List(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ["first_name", "last_name", "email_addr", "phone_number", "subjects_can_help"]       
+        fields = ["pic", "first_name", "last_name", "email_addr", "phone_number", "subjects_can_help"]       
 
 class EditProfile(UserChangeForm):
     class Meta:
