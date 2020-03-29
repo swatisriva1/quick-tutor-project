a3b5841b5b4b83d5f8669f3047160ad0b55d1423
Caleb Bodishbaugh <cbb3jn@virginia.edu>
2020-03-29 16:38:39 -0500

    Added example model test

--- tutor/tests.py
+++ tutor/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase, Client
 from tutor.models import Subject, Profile, Job
 from django.contrib.auth.models import User
+from . import signals
 
 # Create your tests here.
 
@@ -11,13 +12,11 @@ class DummyTestCase(TestCase):
     def test_dummy_test_case(self):
         self.assertEqual(2, 2)
 
-# class ProfileModelTest(TestCase):
-#     def setUp(self):
-#         # creates dummy user object, can be reused in other tests
-#         test_user = User.objects.create_user(username='testuser', password='12345', email='test@gmail.com')
-#         Profile.objects.create(user=test_user, phone_number='4797998232', first_name='test', last_name='user', email_addr='test@gmail.com', rating=4.59)
+class SubjectModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.test_subject = Subject.objects.create(subject_name='test')
 
-#     def test_first_name_max_length(self):
-#         profile = Profile.objects.get(id=1)
-#         max_length = profile._meta.get_field('first_name').max_length
-#         self.assertEquals(max_length, 200)
+    def test_subject_name_max_length(self):
+        self.max_length = self.test_subject._meta.get_field('subject_name').max_length
+        self.assertEquals(self.max_length, 30)
