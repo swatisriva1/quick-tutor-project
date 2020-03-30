7be904440e30afa1903b4848abc79bf62020e30d
swatisriva1 <40495467+swatisriva1@users.noreply.github.com>
2020-03-29 20:30:46 -0400

    Deleted unused import

--- tutor/signals.py
+++ tutor/signals.py
@@ -2,7 +2,7 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import Profile
-from utilities.utils import get_filename, rotate_image
+# from utilities.utils import get_filename, rotate_image
 
 @receiver(post_save, sender=Profile)
 def create_profile(sender, instance, **kwargs):
