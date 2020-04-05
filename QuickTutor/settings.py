ff44f30975b42e2af9a7a8fec8f99a14c7aaef7c
swatisriva1 <40495467+swatisriva1@users.noreply.github.com>
2020-04-05 13:39:58 -0400

    Changed settings to incorporate AWS S3 storage

--- QuickTutor/settings.py
+++ QuickTutor/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'jquery',
     'django_tables2',
     'paypal.standard.ipn',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -167,3 +168,13 @@ LOGOUT_REDIRECT_URL = '/'
 
 SOCIAL_AUTH_URL_NAMESPACE = 'social'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
